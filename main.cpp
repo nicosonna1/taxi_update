0c426e0bd88d563abbd9f8ecab914beeae2abf3d
Nicolas Sonnard <114679645+nicosonna1@users.noreply.github.com>
2022-10-15 04:09:28 +0200

    algo presque fini

--- main.cpp
+++ main.cpp
@@ -14,18 +14,33 @@
 #include <limits>   // numeric_limits<streamsize>
 #include <iomanip>  // setw(...) et setprecision(...)
 
+
+
+#define NICOLAS 1 
+
+
+
 using namespace std;
 
 int main() {
 
-	int tempsCourse = 1440; //distance / vMoyenne * 60; //1440 = 24h
+	int tempsCourse = 27*60; //distance / vMoyenne * 60; //1440 = 24h
 	int nbChangements = tempsCourse / 480; //3changements
+	int tempsNuit;
+	int tempsJour;
+int heure;
+int minute;
+
 
 	int hDepart = 12;
 	int hChangement;
+	int nbJour,
+		jourIncomplet;
 
 	int DEBUT_JOUR = 8;
 	int DEBUT_NUIT = 20;
+	const int TEMP_JOURNEE = DEBUT_NUIT-DEBUT_JOUR;
+	const int TEMP_NUIT =24 - TEMP_JOURNEE;
 	int MINUIT = 24;
 
 	bool estMatinee;
@@ -39,6 +54,57 @@ int main() {
 	int y = 0; //tempsTarifNuit
 	int temp;
 
+	#ifdef NICOLAS
+	
+	cout << "entrer une valeure hh:mm" << endl;
+	//cin >> heure >> minute;
+	heure =12;
+	minute = 12 + heure *60;
+	cout << minute << endl;
+	cout << "temps nuit --> " << TEMP_NUIT << "temps journe " << TEMP_JOURNEE<< endl;
+
+	nbJour=tempsCourse/1440;		// calcul du nombre de jours
+	jourIncomplet=tempsCourse%1440;	// calcule du reste si il y en a un
+	tempsNuit=nbJour*TEMP_NUIT*60;	
+	tempsJour=nbJour*TEMP_JOURNEE*60;
+	if (jourIncomplet!=0)
+	{
+
+		cout << "jour plus grand que 1 --> " << nbJour << "reste  " << jourIncomplet << endl;
+	}
+	else{
+	cout << "jour plus petit que 1 --> " << nbJour << "rest" << jourIncomplet << endl;
+	}
+			
+			if (jourIncomplet>0){
+			// on se calle dans l'intervale de la journée
+			cout << "jour incomplet >0  " << endl;
+			if (minute <DEBUT_NUIT*60 && minute >DEBUT_JOUR*60){
+				cout << "calcule journee --> " << endl;
+				int calcule =   DEBUT_NUIT*60-jourIncomplet;
+
+			// si la diférence entre le temps du rest (jourIncomplet) est plus grande que la journée 
+			// on fait une soustraction pour trouver le temps de nuit
+			if (jourIncomplet<TEMP_JOURNEE*60)
+			{
+
+				cout << "journée plus grande que " << endl;
+				tempsJour+=jourIncomplet;
+			}
+			//a faire pour la nuit 
+			else{
+
+			}
+			 //tempsJour = jourIncomplet - (DEBUT_NUIT*60);
+
+		}
+			}
+
+	cout << "temps jour  --> " << tempsJour/60 << "temps nuit " << tempsNuit/60 << endl;
+
+}
+ 
+	#else
 
 	while (nbChangements > 0) { //3
 
@@ -104,3 +170,4 @@ int main() {
 	cout << "Prix Course = " << prixCourse;
 	return EXIT_SUCCESS;
 }
+#endif
